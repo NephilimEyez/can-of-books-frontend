6c338d295cf68187faf57908f53b926018673158
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-06-20 07:31:42 -0400

    Cors wont let me connect, extension did not help

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
+
   render() {
     return (
       <>
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books:[]
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getAllBooks = async () => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`
+
+      let booksFromDB = await axios.get(url);
+      this.setState({
+        books: booksFromDB.data
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  componentDidMount() {
+    this.getAllBooks();
+  }
 
   render() {
 
@@ -18,10 +35,10 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0? (
+          console.log(this.state.books)
         ) : (
-          <h3>No Books Found :(</h3>
+          <h3>No Books Found :</h3>
         )}
       </>
     )
